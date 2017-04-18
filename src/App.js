e2f874967b465bff6da0ed74abb659da2c977873
Aaron McRuer <a.mcruer@live.com>
2017-04-18 09:47:22 -0500

    basic ajax added

--- src/App.js
+++ src/App.js
@@ -4,29 +4,10 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import {Home} from './components/Home.jsx';
 import {About} from './components/About.jsx';
 import {Characters} from './components/Characters.jsx';
-
-/*
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-*/
 
 class App extends Component {
   render() {
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import axios from 'axios';
+
 
 export class Characters extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      iasipCharacters: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://iasip-backend.herokuapp.com/characters.json`)
+      .then(res => {
+        const iasipCharacters = res.data.data.children.map(obj => obj.data);
+        this.setState({ iasipCharacters });
+      });
+  }
+
   render() {
     return (
       <div>
         <h2>Characters</h2>
+        <ul>
+          {this.state.iasipCharacters.map(char => 
+            <li key={char}>{char.preferred_name}</li>
+          )}
+        </ul>
       </div>
     );
   }
