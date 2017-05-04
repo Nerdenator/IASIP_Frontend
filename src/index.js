7906f944adeb94c658c08a64aad65bbd5cda6c89
Aaron McRuer <a.mcruer@live.com>
2017-05-04 13:04:03 -0500

    need to find way to pass props down to GangMember from parent

--- src/index.js
+++ src/index.js
@@ -9,64 +9,95 @@ import {
 } from 'react-router-dom';
 import axios from 'axios';
 
+const GangMember = (props) => {
+
+    const gangMember = axios.get(`http://iasip-backend.herokuapp.com/character_crime_list/${this.props.state.gangList.member}`)
+        .then(res => {
+            const theGang = res.data.map(obj => obj);
+        });
+};
+
 class TheGang extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
             gangList: []
-        };
+        }
     }
 
     componentDidMount() {
         axios.get(`http://iasip-backend.herokuapp.com/characters/?format=json`)
             .then(res => {
-                //console.log(res.data.map(function(a) {return a.preferred_name;}));
-                const theGang = res.data.map(obj => obj.preferred_name);
-                this.setState({gangList:theGang});
+                console.log(res);
+                const theGang = res.data.map(obj => obj);
+                this.setState({gangList: theGang});
                 console.log(this.state);
-            })
+            });
     }
 
-    render(){
-        return(
-            <ul>
-                {this.state.gangList.map(member =>
-                    <li key={member}><Link to={`/${member.toLowerCase()}`}>{member}</Link></li>
-                )}
-            </ul>
+    render() {
+        return (
+            <div>
+                <h1>TheGang component</h1>
+
+                <ul>
+                    {this.state.gangList.map(member =>
+                        <li key={member.id}>
+                            <Link to={
+                                {
+                                    pathname: `/the_gang/${member.preferred_name.toLowerCase()}`,
+                                    state: { gangList: this.state.gangList }
+                                }
+                            }>{member.preferred_name}</Link>
+                        </li>
+                    )}
+                </ul>
+
+            </div>
         )
     }
 }
 
-const Home = () => (
-    <div>
-        <h1>The Gang Gets Indicted</h1>
-    </div>
+const Gang = () => (
+    <Switch>
+        <Route exact path="/the_gang" component={TheGang}/>
+        <Route path="/the_gang/:member" component={GangMember}/>
+    </Switch>
 );
 
-const Main = () => (
-    <main>
-        <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/gang' component={TheGang} />
-        </Switch>
-    </main>
-);
+const
+    Home = () => (
+        <div>
+            <h1>The Gang Gets Indicted</h1>
+        </div>
+    );
 
-const Header = () => (
-    <header>
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/gang">The Gang</Link></li>
-                <li><Link to="/about">About</Link></li>
-            </ul>
-        </nav>
-    </header>
-);
+const
+    Main = () => (
+        <main>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route path='/the_gang' component={Gang}/>
+            </Switch>
+        </main>
+    );
 
-class App extends Component {
+const
+    Header = () => (
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/the_gang">The Gang</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+
+class App
+    extends Component {
     render() {
         return (
             <div>
